6ee8cfbcb07821d198c3a654c903f57a2bee13eb
Arne Bahlo <hey@arne.me>
2021-09-30 10:45:43 +0200

    Add zap

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/bahlo/mapdns
 
 go 1.17
 
-require github.com/miekg/dns v1.1.43
+require (
+	github.com/miekg/dns v1.1.43
+	go.uber.org/zap v1.19.1
+)
 
 require (
-	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
-	golang.org/x/sys v0.0.0-20210303074136-134d130e1a04 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"log"
+	"fmt"
 	"net"
 	"os"
 	"strings"
 
 	"github.com/miekg/dns"
+	"go.uber.org/zap"
 )
 
 type Entry struct {
@@ -45,17 +46,21 @@ func ReadConfig(fileName string) (Config, error) {
 }
 
 type Handler struct {
-	cfg Config
+	logger *zap.Logger
+	cfg    Config
 }
 
 func (h *Handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	msg := dns.Msg{}
 	msg.SetReply(r)
+
 	switch r.Question[0].Qtype {
 	case dns.TypeA:
 		msg.Authoritative = true
 		domain := msg.Question[0].Name
 		address, ok := h.cfg.Lookup(domain)
+		h.logger.Debug("Looked up domain", zap.String("domain", domain), zap.String("A", address.A))
+
 		if ok && address.A != "" {
 			msg.Answer = append(msg.Answer, &dns.A{
 				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
@@ -63,18 +68,28 @@ func (h *Handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 			})
 		}
 	}
-	w.WriteMsg(&msg)
+
+	if err := w.WriteMsg(&msg); err != nil {
+		h.logger.Error("Failed to write message", zap.Error(err))
+	}
 }
 
 func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, `{"level":"error", "message": "Failed to set up logger", "error": %q}`, err)
+	}
+	defer logger.Sync()
+
 	cfg, err := ReadConfig("mapdns.json")
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("Failed to read config", zap.Error(err))
 	}
 
 	srv := &dns.Server{Addr: ":53", Net: "udp"}
-	srv.Handler = &Handler{cfg: cfg}
+	srv.Handler = &Handler{logger: logger, cfg: cfg}
+
 	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalf("Failed to set udp listener %s\n", err.Error())
+		logger.Error("Failed to set udp listener", zap.Error(err))
 	}
 }
